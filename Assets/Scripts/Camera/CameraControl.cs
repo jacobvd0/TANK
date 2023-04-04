c57e333637f6cd8626aecacc6e9d58ed433320ee
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-04-04 19:25:23 +0930

    camera zoom

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,9 +12,17 @@ public class CameraControl : MonoBehaviour
     private Vector3 m_MoveVelocity;
     private Vector3 m_DesiredPosition;
 
+
+    float cameraSize;
+    float currentSize;
+    float maxSize = 18;
+    float minSize = 1;
+
     private void Awake()
     {
         m_target = GameObject.FindGameObjectWithTag("Player").transform;
+        cameraSize = Camera.main.orthographicSize;
+        currentSize = cameraSize;
     }
 
     private void FixedUpdate()
@@ -25,5 +34,30 @@ public class CameraControl : MonoBehaviour
     {
         m_DesiredPosition = m_target.position;
         transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+    private void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        //Debug.Log(scroll);
+        if (scroll < 0)
+        {
+            if (currentSize <= maxSize)
+            {
+                currentSize -= scroll * 2;
+                Camera.main.orthographicSize = currentSize;
+                Debug.Log(currentSize);
+            }
+            
+        } else if (scroll > 0)
+        {
+            if (currentSize >= minSize)
+            {
+                currentSize -= scroll * 2;
+                Camera.main.orthographicSize = currentSize;
+                Debug.Log(currentSize);
+            }
+            
+        }
     }
 }
